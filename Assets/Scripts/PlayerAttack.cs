cf9e2fd50c4d6aa82da705c350569ddaf54f8a4a
mael,LEMAIRE <mlemaire@gaming.tech>
2026-09-17 14:29:34 +0200

    [PUSH] la mere de loic

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -1,39 +1,30 @@
 using Unity.VisualScripting;
 using UnityEngine;
 
-
 public class PlayerAttack : MonoBehaviour
 {
     GameObject enemy;
 
-    private void Start()
-    {
-    }
-
     public void Attack()
     {
         if (enemy == null)
-        {
-            Debug.Log("null");
             return;
-        }
 
         Health enemyHealth = enemy.GetComponent<Health>();
         if (enemyHealth != null)
-        {
             enemyHealth.health -= 10;
-            Debug.Log("Attack Done");
-        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Enemy")
-        {
             enemy = collision.gameObject;
-            Debug.Log("go recup");
-        }
-        
+    }
+    
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+            enemy = null;
     }
 
 }
