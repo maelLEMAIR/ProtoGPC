1c2309d017ca34260c602b36047deca1b0b724d8
Médérick Leprince Gasser <mleprincegasser@gaming.tech>
2026-09-17 15:23:16 +0200

    protoproto

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -1,9 +1,29 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
+    public Material damageMaterial;
+    public Material enemyMaterial;
+
+    public MeshRenderer enemyRenderer;
+
+    float cooldown = 0f;
+    public float time = 0.05f;
+
     GameObject enemy;
+
+
+    private void Update()
+    {
+        if(cooldown > 0f)
+        {
+            cooldown -= Time.deltaTime;
+        }
+        else
+        {
+            enemyRenderer.material = enemyMaterial;
+        }
+    }
 
     public void Attack()
     {
@@ -11,14 +31,27 @@ public class PlayerAttack : MonoBehaviour
             return;
 
         Health enemyHealth = enemy.GetComponent<Health>();
+
         if (enemyHealth != null)
+        {
             enemyHealth.health -= 10;
+            Debug.Log("attack");
+
+
+            enemyRenderer.material = damageMaterial;
+
+            cooldown = time;
+        }
+
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Enemy")
+        {
             enemy = collision.gameObject;
+            Debug.Log("collision");
+        }
     }
     
     private void OnCollisionExit(Collision collision)
